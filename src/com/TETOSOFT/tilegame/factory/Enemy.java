5cc3ba02304613362b9f6a3c0d66d45a56221c6e
Oumaima <chellyoumaima70@gmail.com>
2025-12-02 23:16:49 +0100

    feat: pattron factory by hadil

---
+++ src/com/TETOSOFT/tilegame/factory/Enemy.java
@@ -0,0 +1,3 @@
+package com.TETOSOFT.tilegame.factory;
+ public interface Enemy 
+ { }
--- src/com/TETOSOFT/tilegame/sprites/Grub.java
+++ src/com/TETOSOFT/tilegame/sprites/Grub.java
@@ -1,11 +1,12 @@
 package com.TETOSOFT.tilegame.sprites;
 
 import com.TETOSOFT.graphics.Animation;
+import com.TETOSOFT.tilegame.factory.Enemy;
 
 /**
     A Grub is a Creature that moves slowly on the ground.
 */
-public class Grub extends Creature {
+public class Grub extends Creature implements Enemy {
 
     public Grub(Animation left, Animation right,
         Animation deadLeft, Animation deadRight)
